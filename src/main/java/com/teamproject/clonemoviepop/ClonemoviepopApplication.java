eeb03c28be2bd9c1d2967ef8b5d0296542daeed7
44Kwon <kwh3217@gmail.com>
2025-02-12 14:40:27 +0900

    first commit

---
+++ src/main/java/com/teamproject/clonemoviepop/ClonemoviepopApplication.java
@@ -0,0 +1,13 @@
+package com.teamproject.clonemoviepop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClonemoviepopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClonemoviepopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teamproject/clonemoviepop/ClonemoviepopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamproject.clonemoviepop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClonemoviepopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
