492d154a77f28cd4efb8544ef95a44468a3ba8d8
44Kwon <kwh3217@gmail.com>
2025-03-09 17:49:32 +0900

    오류 공통화 처리 완료 및 오류 응답 클래스 작성완료

--- src/main/java/com/teamproject/clonemoviepop/advice/GlobalExceptionAdvice.java
+++ src/main/java/com/teamproject/clonemoviepop/advice/GlobalExceptionAdvice.java
@@ -1,4 +1,76 @@
 package com.teamproject.clonemoviepop.advice;
 
+import com.teamproject.clonemoviepop.exception.BusinessLogicException;
+import com.teamproject.clonemoviepop.response.ErrorResponse;
+import jakarta.validation.ConstraintViolationException;
+import org.apache.tomcat.util.http.fileupload.impl.FileSizeLimitExceededException;
+import org.apache.tomcat.util.http.fileupload.impl.SizeLimitExceededException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GlobalExceptionAdvice {
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+        ErrorResponse errorResponse = ErrorResponse.of(exception.getBindingResult());
+        return errorResponse;
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleConstraintViolationException(ConstraintViolationException exception) {
+        ErrorResponse errorResponse = ErrorResponse.of(exception.getConstraintViolations());
+        return errorResponse;
+    }
+
+    @ExceptionHandler
+    public ResponseEntity handleBusinessLogicException(BusinessLogicException exception) {
+        final ErrorResponse errorResponse = ErrorResponse.of(exception.getExceptionCode());
+        return new ResponseEntity(errorResponse, HttpStatus.valueOf(exception.getExceptionCode().getStatus()));
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
+    public ErrorResponse handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException exception) {
+        final ErrorResponse errorResponse = ErrorResponse.of(HttpStatus.METHOD_NOT_ALLOWED);
+        return errorResponse;
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleHttpMessageNotReadableException(HttpMessageNotReadableException exception) {
+        final ErrorResponse errorResponse = ErrorResponse.of(HttpStatus.BAD_REQUEST, "Required request body is missing");
+        return errorResponse;
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleMissingServletRequestParameterException(MissingServletRequestParameterException exception) {
+        final ErrorResponse errorResponse = ErrorResponse.of(HttpStatus.BAD_REQUEST, exception.getMessage());
+        return errorResponse;
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleFileSizeLimitExceededException(FileSizeLimitExceededException exception) {
+        final ErrorResponse errorResponse = ErrorResponse.of(HttpStatus.BAD_REQUEST, "이미지는 최대 5MB까지 업로드 가능합니다.");
+        return errorResponse;
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleSizeLimitExceededException(SizeLimitExceededException exception) {
+        final ErrorResponse errorResponse = ErrorResponse.of(HttpStatus.BAD_REQUEST, "전체 요청 크기가 최대 10MB를 초과하였습니다.");
+        return errorResponse;
+    }
+
 }
--- src/main/java/com/teamproject/clonemoviepop/response/ErrorResponse.java
+++ src/main/java/com/teamproject/clonemoviepop/response/ErrorResponse.java
@@ -1,4 +1,108 @@
 package com.teamproject.clonemoviepop.response;
 
+import com.teamproject.clonemoviepop.exception.ExceptionCode;
+import jakarta.validation.ConstraintViolation;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * 오류 Response
+ */
+@Getter
 public class ErrorResponse {
+    private int status;
+    private String message;
+    private List<FieldError> fieldErrors;
+    private List<ConstraintViolationError> violationErrors;
+
+    public ErrorResponse(int status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    public ErrorResponse(List<FieldError> fieldErrors, List<ConstraintViolationError> violationErrors) {
+        this.fieldErrors = fieldErrors;
+        this.violationErrors = violationErrors;
+    }
+
+    public static ErrorResponse of(HttpStatus status) {
+        return new ErrorResponse(status.value(), status.getReasonPhrase());
+    }
+
+    public static ErrorResponse of(HttpStatus status, String message) {
+        return new ErrorResponse(status.value(), message);
+    }
+
+    public static ErrorResponse of(BindingResult bindingResult) {
+        return new ErrorResponse(FieldError.of(bindingResult),null);
+    }
+
+    public static ErrorResponse of(Set<ConstraintViolation<?>> violations) {
+        return new ErrorResponse(null, ConstraintViolationError.of(violations));
+    }
+
+    public static ErrorResponse of(ExceptionCode exceptionCode) {
+        return new ErrorResponse(exceptionCode.getStatus(),exceptionCode.getMessage());
+    }
+
+    //FieldError는 필드 유효성 검증에서 발생한 오류에 대한 정보를 저장
+    @Getter
+    public static class FieldError {
+        private String field; //오류가 발생한 필드의 이름
+        private Object rejectedValue; //해당 필드에 대해 잘못된 값
+        private String reason; //오류에 대한 설명 메시지
+
+        public FieldError(String field, Object rejectedValue, String reason) {
+            this.field = field;
+            this.rejectedValue = rejectedValue;
+            this.reason = reason;
+        }
+
+        //BindingResult에서 발생한 필드 오류들을 FieldError 객체로 변환하여 리스트로 반환합니다.
+        public static List<FieldError> of(BindingResult bindingResult) {
+            List<org.springframework.validation.FieldError> fieldErrors = bindingResult.getFieldErrors();
+
+            return fieldErrors.stream()
+                    .map(error -> new FieldError(
+                            error.getField(),
+                            error.getRejectedValue(),
+                            error.getDefaultMessage()
+                    ))
+                    .collect(Collectors.toList());
+        }
+    }
+
+    //제약 조건 위반 오류에 대한 정보를 저장합니다.
+    //이는 주로 JSR-303/JSR-380 표준(예: @NotNull, @Size 등) 어노테이션을 사용할 때 발생합니다.
+    @Getter
+    public static class ConstraintViolationError {
+        private String propertyPath; //오류가 발생한 객체의 속성 경로
+        private Object rejectedValue; // 잘못된 값
+        private String reason; //오류에 대한 설명 메시지
+
+        public ConstraintViolationError(String propertyPath, Object rejectedValue, String reason) {
+            this.propertyPath = propertyPath;
+            this.rejectedValue = rejectedValue;
+            this.reason = reason;
+        }
+
+        //제약 위반 오류들을 ConstraintViolationError 객체로 변환하여 리스트로 반환합니다.
+        //Set<ConstraintViolation<?>>**에서 오류를 추출
+        public static List<ConstraintViolationError> of(Set<ConstraintViolation<?>> constraintViolations) {
+            return constraintViolations.stream()
+                    .map(error -> new ConstraintViolationError(
+                            error.getPropertyPath().toString(),
+                            error.getInvalidValue(),
+                            error.getMessage()
+                    ))
+                    .collect(Collectors.toList());
+        }
+    }
 }
+
